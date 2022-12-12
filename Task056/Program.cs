9c559c31fd2b96358fc2f725099efa579f88fe0c
Tra-Ta <madtazik@gmail.com>
2022-12-12 22:47:03 +0300

    Complete Task056

---
+++ Task056/Program.cs
@@ -0,0 +1,75 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. 
+// Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+int[,] CreateMatrixRndInt(int line, int columns)
+{
+    int[,] matrix = new int[line, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(0, 10);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],5}, ");
+            else Console.Write($"{matrix[i, j],5}");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+int[] SumMatrixRows(int[,] matrix)
+{
+    int[] array = new int[matrix.GetLength(0)];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            sum += matrix[i, j];
+        }
+        array[i] = sum;
+    }
+    return array;
+}
+
+
+int MinSumElementsRows(int[] array)
+{
+    int min = array[0];
+    int index = 0;
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (min > array[i])
+        {
+            min = array[i];
+            index = i;
+        }
+    }
+    return index;
+}
+int[,] matrix = CreateMatrixRndInt(4, 3);
+PrintMatrix(matrix);
+int[] array2D = SumMatrixRows(matrix);
+Console.WriteLine(" ");
+int result = MinSumElementsRows(array2D);
+Console.WriteLine($"The number of the row with the smallest sum of elements -> {result + 1} line");
